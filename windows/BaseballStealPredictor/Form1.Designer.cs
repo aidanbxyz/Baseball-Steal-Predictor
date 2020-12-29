faccaf22ad77b739b1ce0587da07931cf0081079
aidanbxyz <43518094+aidanbxyz@users.noreply.github.com>
2020-12-28 21:02:18 -0600

    working on windows exe.. (NOT DONE)

--- windows/BaseballStealPredictor/Form1.Designer.cs
+++ windows/BaseballStealPredictor/Form1.Designer.cs
@@ -50,214 +50,217 @@
             this.label8 = new System.Windows.Forms.TextBox();
             this.label7 = new System.Windows.Forms.TextBox();
             this.label6 = new System.Windows.Forms.TextBox();
-            this.submitLabels = new System.Windows.Forms.Button();
             this.labellabels = new System.Windows.Forms.Label();
             this.title = new System.Windows.Forms.Label();
+            this.possibilitiesTitle = new System.Windows.Forms.Label();
+            this.randombar = new System.Windows.Forms.ProgressBar();
             this.SuspendLayout();
             // 
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(107, 104);
+            this.button1.Location = new System.Drawing.Point(121, 104);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(40, 40);
             this.button1.TabIndex = 0;
             this.button1.Text = "1";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
             this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(153, 104);
+            this.button2.Location = new System.Drawing.Point(167, 104);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(40, 40);
             this.button2.TabIndex = 1;
             this.button2.Text = "2";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
             this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button3.Location = new System.Drawing.Point(199, 104);
+            this.button3.Location = new System.Drawing.Point(213, 104);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(40, 40);
             this.button3.TabIndex = 2;
             this.button3.Text = "3";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button4
             // 
             this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button4.Location = new System.Drawing.Point(107, 150);
+            this.button4.Location = new System.Drawing.Point(121, 150);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(40, 40);
             this.button4.TabIndex = 3;
             this.button4.Text = "4";
             this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // button5
             // 
             this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button5.Location = new System.Drawing.Point(153, 150);
+            this.button5.Location = new System.Drawing.Point(167, 150);
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(40, 40);
             this.button5.TabIndex = 4;
             this.button5.Text = "5";
             this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
             // button6
             // 
             this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button6.Location = new System.Drawing.Point(199, 150);
+            this.button6.Location = new System.Drawing.Point(213, 150);
             this.button6.Name = "button6";
             this.button6.Size = new System.Drawing.Size(40, 40);
             this.button6.TabIndex = 5;
             this.button6.Text = "6";
             this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
             // 
             // button7
             // 
             this.button7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button7.Location = new System.Drawing.Point(107, 196);
+            this.button7.Location = new System.Drawing.Point(121, 196);
             this.button7.Name = "button7";
             this.button7.Size = new System.Drawing.Size(40, 40);
             this.button7.TabIndex = 6;
             this.button7.Text = "7";
             this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
             // 
             // button8
             // 
             this.button8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button8.Location = new System.Drawing.Point(153, 196);
+            this.button8.Location = new System.Drawing.Point(167, 196);
             this.button8.Name = "button8";
             this.button8.Size = new System.Drawing.Size(40, 40);
             this.button8.TabIndex = 7;
             this.button8.Text = "8";
             this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
             // 
             // button9
             // 
             this.button9.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button9.Location = new System.Drawing.Point(199, 196);
+            this.button9.Location = new System.Drawing.Point(213, 196);
             this.button9.Name = "button9";
             this.button9.Size = new System.Drawing.Size(40, 40);
             this.button9.TabIndex = 8;
             this.button9.Text = "9";
             this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
             // 
             // button0
             // 
             this.button0.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button0.Location = new System.Drawing.Point(107, 242);
+            this.button0.Location = new System.Drawing.Point(121, 242);
             this.button0.Name = "button0";
             this.button0.Size = new System.Drawing.Size(40, 40);
             this.button0.TabIndex = 9;
             this.button0.Text = "0";
             this.button0.UseVisualStyleBackColor = true;
+            this.button0.Click += new System.EventHandler(this.button0_Click);
             // 
             // buttonback
             // 
             this.buttonback.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.buttonback.Location = new System.Drawing.Point(153, 242);
+            this.buttonback.Location = new System.Drawing.Point(167, 242);
             this.buttonback.Name = "buttonback";
             this.buttonback.Size = new System.Drawing.Size(86, 40);
             this.buttonback.TabIndex = 10;
             this.buttonback.Text = "<-- BACK";
             this.buttonback.UseVisualStyleBackColor = true;
+            this.buttonback.Click += new System.EventHandler(this.buttonback_Click);
             // 
             // maintextbox
             // 
-            this.maintextbox.Location = new System.Drawing.Point(107, 78);
+            this.maintextbox.Location = new System.Drawing.Point(121, 78);
             this.maintextbox.Name = "maintextbox";
             this.maintextbox.ReadOnly = true;
             this.maintextbox.Size = new System.Drawing.Size(132, 20);
             this.maintextbox.TabIndex = 11;
+            this.maintextbox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.maintextbox_KeyDown);
             // 
             // label1
             // 
-            this.label1.Location = new System.Drawing.Point(488, 78);
+            this.label1.Location = new System.Drawing.Point(549, 78);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(100, 20);
             this.label1.TabIndex = 12;
             // 
             // label2
             // 
-            this.label2.Location = new System.Drawing.Point(488, 104);
+            this.label2.Location = new System.Drawing.Point(549, 104);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(100, 20);
             this.label2.TabIndex = 13;
             // 
             // label3
             // 
-            this.label3.Location = new System.Drawing.Point(488, 131);
+            this.label3.Location = new System.Drawing.Point(549, 131);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(100, 20);
             this.label3.TabIndex = 14;
             // 
             // label4
             // 
-            this.label4.Location = new System.Drawing.Point(488, 158);
+            this.label4.Location = new System.Drawing.Point(549, 158);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(100, 20);
             this.label4.TabIndex = 15;
             // 
             // label5
             // 
-            this.label5.Location = new System.Drawing.Point(488, 185);
+            this.label5.Location = new System.Drawing.Point(549, 185);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(100, 20);
             this.label5.TabIndex = 16;
             // 
             // label0
             // 
-            this.label0.Location = new System.Drawing.Point(488, 316);
+            this.label0.Location = new System.Drawing.Point(549, 316);
             this.label0.Name = "label0";
             this.label0.Size = new System.Drawing.Size(100, 20);
             this.label0.TabIndex = 21;
             // 
             // label9
             // 
-            this.label9.Location = new System.Drawing.Point(488, 290);
+            this.label9.Location = new System.Drawing.Point(549, 290);
             this.label9.Name = "label9";
             this.label9.Size = new System.Drawing.Size(100, 20);
             this.label9.TabIndex = 20;
             // 
             // label8
             // 
-            this.label8.Location = new System.Drawing.Point(488, 264);
+            this.label8.Location = new System.Drawing.Point(549, 264);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(100, 20);
             this.label8.TabIndex = 19;
             // 
             // label7
             // 
-            this.label7.Location = new System.Drawing.Point(488, 237);
+            this.label7.Location = new System.Drawing.Point(549, 237);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(100, 20);
             this.label7.TabIndex = 18;
             // 
             // label6
             // 
-            this.label6.Location = new System.Drawing.Point(488, 211);
+            this.label6.Location = new System.Drawing.Point(549, 211);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(100, 20);
             this.label6.TabIndex = 17;
-            // 
-            // submitLabels
-            // 
-            this.submitLabels.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.submitLabels.Location = new System.Drawing.Point(621, 185);
-            this.submitLabels.Name = "submitLabels";
-            this.submitLabels.Size = new System.Drawing.Size(119, 46);
-            this.submitLabels.TabIndex = 22;
-            this.submitLabels.Text = "REGISTER";
-            this.submitLabels.UseVisualStyleBackColor = true;
             // 
             // labellabels
             // 
             this.labellabels.AutoSize = true;
             this.labellabels.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labellabels.Location = new System.Drawing.Point(457, 83);
+            this.labellabels.Location = new System.Drawing.Point(518, 83);
             this.labellabels.Name = "labellabels";
             this.labellabels.Size = new System.Drawing.Size(25, 250);
             this.labellabels.TabIndex = 23;
@@ -273,14 +276,32 @@
             this.title.TabIndex = 24;
             this.title.Text = "Baseball Steal Predictor";
             // 
+            // possibilitiesTitle
+            // 
+            this.possibilitiesTitle.AutoSize = true;
+            this.possibilitiesTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.possibilitiesTitle.Location = new System.Drawing.Point(220, 408);
+            this.possibilitiesTitle.Name = "possibilitiesTitle";
+            this.possibilitiesTitle.Size = new System.Drawing.Size(361, 37);
+            this.possibilitiesTitle.TabIndex = 25;
+            this.possibilitiesTitle.Text = "Steal Sign Possibilities";
+            // 
+            // randombar
+            // 
+            this.randombar.Location = new System.Drawing.Point(218, 448);
+            this.randombar.Name = "randombar";
+            this.randombar.Size = new System.Drawing.Size(364, 23);
+            this.randombar.TabIndex = 26;
+            // 
             // mainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 667);
+            this.Controls.Add(this.randombar);
+            this.Controls.Add(this.possibilitiesTitle);
             this.Controls.Add(this.title);
             this.Controls.Add(this.labellabels);
-            this.Controls.Add(this.submitLabels);
             this.Controls.Add(this.label0);
             this.Controls.Add(this.label9);
             this.Controls.Add(this.label8);
@@ -335,9 +356,10 @@
         private System.Windows.Forms.TextBox label8;
         private System.Windows.Forms.TextBox label7;
         private System.Windows.Forms.TextBox label6;
-        private System.Windows.Forms.Button submitLabels;
         private System.Windows.Forms.Label labellabels;
         private System.Windows.Forms.Label title;
+        private System.Windows.Forms.Label possibilitiesTitle;
+        private System.Windows.Forms.ProgressBar randombar;
     }
 }
 
--- windows/BaseballStealPredictor/Form1.cs
+++ windows/BaseballStealPredictor/Form1.cs
@@ -15,5 +15,118 @@ namespace BaseballStealPredictor
         {
             InitializeComponent();
         }
+
+        public static class Globals
+        {
+            public static List<string> mainarray = new List<string>();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            maintextbox.Text += '1';
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            maintextbox.Text += '2';
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            maintextbox.Text += '3';
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            maintextbox.Text += '4';
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            maintextbox.Text += '5';
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            maintextbox.Text += '6';
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            maintextbox.Text += '7';
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            maintextbox.Text += '8';
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            maintextbox.Text += '9';
+        }
+
+        private void button0_Click(object sender, EventArgs e)
+        {
+            maintextbox.Text += '0';
+        }
+
+        private void buttonback_Click(object sender, EventArgs e)
+        {
+            maintextbox.Text = maintextbox.Text.Remove(maintextbox.Text.Length - 1);
+        }
+
+        private void maintextbox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode.ToString().Contains("1"))
+            {
+                maintextbox.Text += '1';
+            }
+            if (e.KeyCode.ToString().Contains("2"))
+            {
+                maintextbox.Text += '2';
+            }
+            if (e.KeyCode.ToString().Contains("3"))
+            {
+                maintextbox.Text += '3';
+            }
+            if (e.KeyCode.ToString().Contains("4"))
+            {
+                maintextbox.Text += '4';
+            }
+            if (e.KeyCode.ToString().Contains("5"))
+            {
+                maintextbox.Text += '5';
+            }
+            if (e.KeyCode.ToString().Contains("6"))
+            {
+                maintextbox.Text += '6';
+            }
+            if (e.KeyCode.ToString().Contains("7"))
+            {
+                maintextbox.Text += '7';
+            }
+            if (e.KeyCode.ToString().Contains("8"))
+            {
+                maintextbox.Text += '8';
+            }
+            if (e.KeyCode.ToString().Contains("9"))
+            {
+                maintextbox.Text += '9';
+            }
+            if (e.KeyCode.ToString().Contains("0"))
+            {
+                maintextbox.Text += '0';
+            }
+            if (e.KeyCode.ToString() == "Back")
+            {
+                maintextbox.Text = maintextbox.Text.Remove(maintextbox.Text.Length - 1);
+            }
+        }
+
+        public string listToString(List<string> theList)
+        {
+            return string.Join(",", theList.ToArray());
+        }
     }
 }
